031ec04bd098d890edbd0ecb7bbacec40425b2a7
ClaraMars <clara.marquez.torres@gmail.com>
2023-06-12 21:14:13 +0200

    Welcome not responsive

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,18 @@
+import classes from "./Welcome.module.css";
+import WelcomeImage from "../../assets/welcome.webp";
+
+const Welcome = () => {
+    return (
+        <section className={classes["welcome-section"]}>
+            <div className={classes["welcome-text"]}>
+                <h3>WELCOME TO LITHO <strong>CAFE & RESTURANT</strong></h3>
+                <p className={classes.subtitle}>One should not attend even the end of the world without a good breakfast</p>
+                <p className={classes.lorem}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam at quas odit ab minima quae odio et dolorum neque facilis quos quisquam porro, exercitationem quo libero ipsa ut dolor.</p>
+                <button>EXPLORE MORE</button>
+            </div>
+            <img className={classes["welcome-image"]} src={WelcomeImage} alt="Welcome to Litho Cafe & Restaurant"/>
+        </section>
+    );
+}
+
+export default Welcome;
