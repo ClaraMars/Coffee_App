42841046a3d542d44cbf2666115c1eb8958de288
ClaraMars <clara.marquez.torres@gmail.com>
2023-06-14 01:02:35 +0200

    Welcome finished

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -3,15 +3,20 @@ import WelcomeImage from "../../assets/welcome.webp";
 
 const Welcome = () => {
     return (
+        <>
         <section className={classes["welcome-section"]}>
+            <img className={classes["welcome-image"]} src={WelcomeImage} alt="Welcome to Litho Cafe & Restaurant"/>
             <div className={classes["welcome-text"]}>
                 <h3>WELCOME TO LITHO <strong>CAFE & RESTURANT</strong></h3>
                 <p className={classes.subtitle}>One should not attend even the end of the world without a good breakfast</p>
-                <p className={classes.lorem}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam at quas odit ab minima quae odio et dolorum neque facilis quos quisquam porro, exercitationem quo libero ipsa ut dolor.</p>
+                <p className={classes.lorem}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui totam at quas odit ab minima quae odio et dolorum neque facilis quos quisquam porro.</p>
                 <button>EXPLORE MORE</button>
             </div>
-            <img className={classes["welcome-image"]} src={WelcomeImage} alt="Welcome to Litho Cafe & Restaurant"/>
         </section>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quasi consequatur debitis voluptatibus ratione, nisi dolorem quaerat soluta velit. Laborum mollitia maxime dicta dolores sint eveniet cum tempore culpa, consequuntur sunt magni perferendis quis perspiciatis tempora, blanditiis, repellendus laboriosam suscipit ipsa distinctio at hic optio eligendi. Blanditiis modi a explicabo quae, quod aspernatur veniam ex harum. Numquam, assumenda. Iusto tempora voluptas repudiandae rerum corporis sint assumenda voluptatibus omnis facere atque nesciunt necessitatibus, nemo quasi nobis, officiis earum! Veritatis maxime vero suscipit, pariatur impedit quibusdam quam. Laborum ratione placeat itaque alias iusto enim accusantium modi, perferendis animi, dolorum voluptas, optio quisquam?</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quasi consequatur debitis voluptatibus ratione, nisi dolorem quaerat soluta velit. Laborum mollitia maxime dicta dolores sint eveniet cum tempore culpa, consequuntur sunt magni perferendis quis perspiciatis tempora, blanditiis, repellendus laboriosam suscipit ipsa distinctio at hic optio eligendi. Blanditiis modi a explicabo quae, quod aspernatur veniam ex harum. Numquam, assumenda. Iusto tempora voluptas repudiandae rerum corporis sint assumenda voluptatibus omnis facere atque nesciunt necessitatibus, nemo quasi nobis, officiis earum! Veritatis maxime vero suscipit, pariatur impedit quibusdam quam. Laborum ratione placeat itaque alias iusto enim accusantium modi, perferendis animi, dolorum voluptas, optio quisquam?</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quasi consequatur debitis voluptatibus ratione, nisi dolorem quaerat soluta velit. Laborum mollitia maxime dicta dolores sint eveniet cum tempore culpa, consequuntur sunt magni perferendis quis perspiciatis tempora, blanditiis, repellendus laboriosam suscipit ipsa distinctio at hic optio eligendi. Blanditiis modi a explicabo quae, quod aspernatur veniam ex harum. Numquam, assumenda. Iusto tempora voluptas repudiandae rerum corporis sint assumenda voluptatibus omnis facere atque nesciunt necessitatibus, nemo quasi nobis, officiis earum! Veritatis maxime vero suscipit, pariatur impedit quibusdam quam. Laborum ratione placeat itaque alias iusto enim accusantium modi, perferendis animi, dolorum voluptas, optio quisquam?</div>
+        </>
     );
 }
 
